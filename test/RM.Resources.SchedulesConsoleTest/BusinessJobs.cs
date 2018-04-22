8a9fa14d95172484b91e42622ba2d9c234d78620
Rômulo Casa <rm_romulomartins@hotmail.com>
2018-04-22 01:14:20 -0300

    adding the sample schedule project

---
+++ test/RM.Resources.SchedulesConsoleTest/BusinessJobs.cs
@@ -0,0 +1,16 @@
+﻿
+namespace RM.Resources.SchedulesConsoleTest
+{
+    using Schedules;
+    using System.Diagnostics;
+    public static class BusinessJobs
+    {
+        private static Scheduler _scheduler = new Scheduler();
+
+        public static void JobOpenNotepadInstance(Job job)
+        {
+            Process.Start("notepad.exe");
+        }
+
+    }
+}
---
+++ test/RM.Resources.SchedulesConsoleTest/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace RM.Resources.SchedulesConsoleTest
+{
+    using Schedules;
+    using System;
+
+    class Program
+    {
+        private static Scheduler _scheduler = new Scheduler();
+
+        static void Main(string[] args)
+        {
+
+            DateTime dt = DateTime.Now;
+
+            Job openNotepadJob = new Job("JobOpenNotepadInstanceKey");
+            openNotepadJob.Run.Every.Seconds(15);
+            openNotepadJob.StartTime = dt;
+
+            _scheduler.SubmitJob(openNotepadJob, BusinessJobs.JobOpenNotepadInstance);
+
+            Console.WriteLine("Waiting...");
+            Console.ReadKey();
+        }
+    }
+}
